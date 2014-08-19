8755a6fdc48e5d3cf97c8217985d50043b992c03
davieghost <davidjohnson1297@gmail.com>
2014-08-19 14:59:12 -0700

    cleaning up

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,28 @@ $(document).ready(function(){
 
 	var $moveOne = $('#three'),
         $moveTwo = $('#two');
-
-    var $link = $('a');
   
     var   r = 0, 
         dir = true;
 
   	
 
-	    $moveOne.click(function(e) {
+	    $('.enable2').unbind().click(function(e) {
 	      if (e.target !==this)
 	        return;
-
+		console.log('clicked');
 	        dir = !dir;
 	        r = dir? -625 : 0;
 	        $moveOne.stop().animate({right: r+'px'}, 400);
-
+	        
 	    });
-	   $moveTwo.click(function(f) {
+	   $('.enable1').unbind().click(function(f) {
 	     if (f.target !==this)
 	        return;
+	    console.log('clicked');
 	        dir = !dir;
 	        r = dir? -625 : 0;
 	        $moveTwo.stop().animate({right: r+'px'}, 400);
-
+	        
 	    });
+});
