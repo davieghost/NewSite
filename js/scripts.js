ec0efe0437b841e8a683011946898707f4ae6dd7
davieghost <davidjohnson1297@gmail.com>
2014-08-27 00:01:14 -0700

    inital produciton

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+	var $moveOne = $('#three'),
+        $moveTwo = $('#two');
+  
+    var   r = 0, 
+        dir = true,
+        fir = true;
+
+  	
+
+	    $('.enable2').unbind().click(function(e) {
+	      if (e.target !==this)
+	        return;
+		console.log('clicked');
+	        dir = !dir;
+	        r = dir? -625 : 0;
+	        $moveOne.stop().animate({right: r+'px'}, 400);
+	        
+	    });
+	   $('.enable1').unbind().click(function(f) {
+	     if (f.target !==this)
+	        return;
+	    console.log('clicked');
+	        fir = !fir;
+	        r = fir? -625 : 0;
+	        $moveTwo.stop().animate({right: r+'px'}, 400);
+	        
+	    });
+});
